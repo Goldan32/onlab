22fdfb6f68054f34160e533f3567ecbe85911bed
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2021-05-01 20:37:08 +0200

    Added important changes to gnuplot_try.py

--- BeagleBone_scripts/mycodes/gnuplot_try.py
+++ BeagleBone_scripts/mycodes/gnuplot_try.py
@@ -1,7 +1,16 @@
 import PyGnuplot as gp, numpy as np
+from datetime import datetime
 
+
+#now = datetime.now()
+#f = open('temperature.txt', 'a')
+#f.write(now.strftime('%H:%M') + ' ' + str(22.5) + '\n')
 
 gp.c('set term "png"')
 gp.c('set output "myfig.png"')
-gp.c('plot sin(x)')
-#gp.pdf('myfigure.pdf')
+gp.c('set xdata time')
+gp.c('set timefmt "%H:%M"')
+gp.c('set format x "%H:%M"')
+gp.c('set yrange [18:25]')
+gp.c('set xtics rotate by 300')
+gp.c('plot "< tail -n 6 temperature.txt" using 1:2 lt rgb "red" w lp')
